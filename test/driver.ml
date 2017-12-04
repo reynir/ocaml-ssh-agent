d61889a1c20f88035df81083f34c83090fd3071a
Reynir Björnsson <rbj@isnordic.dk>
2017-12-04 22:26:25 +0100

    Implement ssh-agent-unix

--- test/driver.ml
+++ test/driver.ml
@@ -18,66 +18,43 @@ let main () =
     match Sys.getenv_opt "SSH_AUTH_SOCK" with
     | Some path -> path
     | None -> failwith "$SSH_AUTH_SOCK not set" in
-  let fd = Lwt_unix.socket Unix.PF_UNIX Unix.SOCK_STREAM 0 in
-  let%lwt () = Lwt_io.printf "Connecting to %s...\n" sock_path in
-  let%lwt () = Lwt_unix.connect fd Unix.(ADDR_UNIX sock_path) in
-  let inc = Lwt_io.of_fd Lwt_io.input fd in
-  let outc = Lwt_io.of_fd Lwt_io.output fd in
-  let%lwt () = Lwt_io.printf "Connected!\n" in
-  (*let%lwt () = Lwt_cstruct.complete (Lwt_cstruct.write fd) @@
-    cstruct_of_ssh_agent_request Ssh_agentc_request_identities in *)
-  let%lwt () = Lwt_io.write outc
-      (with_faraday (fun buf ->
-           Serialize.write_ssh_agent_request buf Ssh_agentc_request_identities)) in
-  let%lwt pubkeys =
-    match%lwt Angstrom_lwt_unix.parse
-                Ssh_agent.Parse.ssh_agent_message
-                inc with
-    | unconsumed, Ok (Any_response (Ssh_agent_identities_answer pubkeys)) ->
-      let%lwt () = Lwt_io.printf "%d keys\n" (List.length pubkeys) in
-      let%lwt () = Lwt_list.iter_s (fun Ssh_agent.Pubkey.{ comment; pubkey } ->
-          Lwt_io.printf "Key type %s with comment %s\n"
-            (pubkey_type_name pubkey) comment)
-          pubkeys in
-      Lwt.return pubkeys
-    | unconsumed, Ok _ ->
-      Lwt.fail_with "Error: Unexpected ssh-agent response\n"
-    | unconsumed, Error e ->
-      Lwt.fail_with (Printf.sprintf "Error: %s\n" e) in
-  let%lwt () = Lwt_io.write outc
-      (with_faraday (fun buf ->
-           Serialize.write_ssh_agent_request buf Ssh_agentc_remove_all_identities)) in
-  let%lwt () =
-    match%lwt Angstrom_lwt_unix.parse
-                Ssh_agent.Parse.ssh_agent_message
-                inc with
-    | unconsumed, Ok (Any_response Ssh_agent_success) ->
-      Lwt_io.printl "Success!"
-    | unconsumed, Ok (Any_response Ssh_agent_failure) ->
-      Lwt_io.printl "Failure!"
-    | unconsumed, Ok _ ->
-      Lwt_io.eprintf "Error: Unexpected ssh-agent response\n"
-    | unconsumed, Error e ->
-      Lwt_io.eprintf "Error: %s\n" e in
-  (*let%lwt () = Lwt_cstruct.complete (Lwt_cstruct.write fd) @@
-    cstruct_of_ssh_agent_request Ssh_agentc_request_identities in*)
-  let%lwt () = Lwt_io.write outc
-      (with_faraday (fun buf ->
-           Serialize.write_ssh_agent_request buf Ssh_agentc_request_identities)) in
-  let%lwt () =
-    match%lwt Angstrom_lwt_unix.parse
-                Ssh_agent.Parse.ssh_agent_message
-                inc with
-    | unconsumed, Ok (Any_response (Ssh_agent_identities_answer pubkeys)) ->
-      let%lwt () = Lwt_io.printf "%d keys\n" (List.length pubkeys) in
-      Lwt_list.iter_s (fun Ssh_agent.Pubkey.{ comment; pubkey } ->
-          Lwt_io.printf "Key type %s with comment %s\n"
+  let fd = Unix.socket Unix.PF_UNIX Unix.SOCK_STREAM 0 in
+  let () = Printf.printf "Connecting to %s...\n" sock_path in
+  let () = Unix.connect fd Unix.(ADDR_UNIX sock_path) in
+  let ic = Unix.in_channel_of_descr fd in
+  let oc = Unix.out_channel_of_descr fd in
+  let () = Printf.printf "Connected!\n" in
+  let () =
+    match Ssh_agent_unix.request (ic, oc) Ssh_agentc_request_identities with
+    | Ok (Ssh_agent_identities_answer pubkeys) ->
+      let () = Printf.printf "%d keys\n" (List.length pubkeys) in
+      List.iter (fun Ssh_agent.Pubkey.{ comment; pubkey } ->
+          Printf.printf "Key type %s with comment %s\n"
             (pubkey_type_name pubkey) comment)
         pubkeys
-    | unconsumed, Ok _ ->
-      Lwt_io.eprintf "Error: Unexpected ssh-agent response\n"
-    | unconsumed, Error e ->
-      Lwt_io.eprintf "Error: %s\n" e in
-  Lwt.return_unit
+    | Ok (Ssh_agent_failure) ->
+      failwith "Error: ssh-agent failure\n"
+    | Error e ->
+      failwith (Printf.sprintf "Error: %s\n" e) in
+  let () = match Ssh_agent_unix.request (ic, oc)
+                   Ssh_agentc_remove_all_identities with
+  | Ok Ssh_agent_success ->
+    print_endline "Success!"
+  | Ok Ssh_agent_failure ->
+    print_endline "Failure!"
+  | Error e ->
+    Printf.printf "Error: %s\n" e in
+  let () = match Ssh_agent_unix.request (ic, oc) Ssh_agentc_request_identities with
+    | Ok (Ssh_agent_identities_answer pubkeys) ->
+      let () = Printf.printf "%d keys\n" (List.length pubkeys) in
+      List.iter (fun Ssh_agent.Pubkey.{ comment; pubkey } ->
+          Printf.printf "Key type %s with comment %s\n"
+            (pubkey_type_name pubkey) comment)
+        pubkeys
+    | Ok Ssh_agent_failure ->
+      Printf.eprintf "Error: Unexpected ssh-agent response\n"
+    | Error e ->
+      Printf.eprintf "Error: %s\n" e in
+  ()
 
-let () = Lwt_main.run @@ main ()
+let () = main ()
---
+++ unix/ssh_agent_unix.ml
@@ -0,0 +1,61 @@
+let request ((ic, oc) : in_channel * out_channel)
+    (type a)
+    (request : a Ssh_agent.ssh_agent_request)
+  : (a Ssh_agent.ssh_agent_response, string) result =
+  let () =
+    let buf = Faraday.create 1024 in
+    Ssh_agent.Serialize.write_ssh_agent_request buf request;
+    output_string oc (Faraday.serialize_to_string buf);
+    flush oc in
+  match Angstrom_unix.parse Ssh_agent.Parse.ssh_agent_message ic with
+  | { len = 0; _ }, Ok response ->
+    let open Ssh_agent in
+    let success_or_fail (resp : Ssh_agent.any_ssh_agent_response)
+      : ([`Ssh_agentc_successable] Ssh_agent.ssh_agent_response, string) result =
+      match resp with
+      | Any_response (Ssh_agent_success as r) -> Ok r
+      | Any_response (Ssh_agent_failure as r) -> Ok r
+      | Any_response _ -> Error "Illegal response type"
+    in
+    begin match request with
+     | Ssh_agent.Ssh_agentc_request_identities ->
+       begin match (response : any_ssh_agent_response) with
+         | Any_response (Ssh_agent_identities_answer _ as r) ->
+           Ok r
+         | Any_response (Ssh_agent_failure as r) ->
+           Ok r
+         | _ ->
+           Error "Illegal response type"
+       end
+     | Ssh_agent.Ssh_agentc_sign_request (_,_,_) ->
+       begin match response with
+         | Any_response (Ssh_agent_sign_response _ as r) ->
+           Ok r
+         | Any_response (Ssh_agent_failure as r) ->
+           Ok r
+         | _ ->
+           Error "Illegal response type"
+       end
+     | Ssh_agent.Ssh_agentc_extension _ ->
+       begin match response with
+         | Any_response (Ssh_agent_extension_failure as r) ->
+           Ok r
+         | Any_response (Ssh_agent_failure as r) ->
+           Ok r
+         | _ ->
+           Error "Illegal response type"
+       end
+     | Ssh_agent.Ssh_agentc_add_identity _ -> success_or_fail response
+     | Ssh_agent.Ssh_agentc_remove_identity _ -> success_or_fail response
+     | Ssh_agent.Ssh_agentc_remove_all_identities -> success_or_fail response
+     | Ssh_agent.Ssh_agentc_add_smartcard_key _ -> success_or_fail response
+     | Ssh_agent.Ssh_agentc_remove_smartcard_key _ -> success_or_fail response
+     | Ssh_agent.Ssh_agentc_lock _ -> success_or_fail response
+     | Ssh_agent.Ssh_agentc_unlock _ -> success_or_fail response
+     | Ssh_agent.Ssh_agentc_add_id_constrained _ -> success_or_fail response
+     | Ssh_agent.Ssh_agentc_add_smartcard_key_constrained _ -> success_or_fail response
+    end
+  | { len; _ }, Ok _ ->
+    Error "Additional data in reply"
+  | _, Error e ->
+    Error ("Parse error: " ^ e)
