d8588b134ffec213ee7f32e6fb330c01af9833d5
Reynir Björnsson <reynir@reynir.dk>
2017-12-23 01:54:03 +0100

    Fix signature for real

--- src/ssh_agent.ml
+++ src/ssh_agent.ml
@@ -13,16 +13,16 @@ let sign priv (sign_flags : Protocol_number.sign_flag list) blob =
   | Privkey.Ssh_dss key ->
     failwith "Not implemented :-("
   | Privkey.Ssh_rsa key ->
-    let to_sign =
+    let alg_string, to_sign =
       if List.mem Protocol_number.SSH_AGENT_RSA_SHA2_512 sign_flags
       then let digest = Nocrypto.Hash.SHA512.digest (Cstruct.of_string blob) in
-        Cstruct.append Util.id_sha512 digest
+        "rsa-sha2-512", Cstruct.append Util.id_sha512 digest
       else if List.mem Protocol_number.SSH_AGENT_RSA_SHA2_256 sign_flags
       then let digest = Nocrypto.Hash.SHA256.digest (Cstruct.of_string blob) in
-        Cstruct.append Util.id_sha256 digest
+        "rsa-sha2-256", Cstruct.append Util.id_sha256 digest
       else let digest = Nocrypto.Hash.SHA1.digest (Cstruct.of_string blob) in
-        Cstruct.append Util.id_sha1 digest in
+        "ssh-rsa", Cstruct.append Util.id_sha1 digest in
     let signed = Nocrypto.Rsa.PKCS1.sig_encode ~key to_sign in
     Serialize.(with_faraday (fun t ->
-        Wire.write_string t "ssh-rsa";
+        Wire.write_string t alg_string;
         Wire.write_string t (Cstruct.to_string signed)))
